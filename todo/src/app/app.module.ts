41e7e2f6ca4acf57a3efeee76ab61dfc5dbb1fdc
JuniorCriste <informaticode@gmail.com>
2021-11-23 21:34:00 -0300

    0.01

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TodoService } from './services/todo.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -11,7 +12,9 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  TodoService
+],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- todo/src/app/services/todo.service.ts
+++ todo/src/app/services/todo.service.ts
@@ -5,5 +5,35 @@ import { Injectable } from '@angular/core';
 })
 export class TodoService {
 
+
+  private todos: Todo[] = [];
+
   constructor() { }
+
+  public getTodo() : Todo[] {
+    return this.todos;
+  }
+
+  public addTodo() {
+  
+  }
+  
+  public delTodo() {
+  
+  }
+  
+  public updateTodo() {
+  
+  }
+  
+
+
+}
+
+
+
+interface Todo {
+  value: string;
+  date: Date;
+  done?: boolean;
 }
