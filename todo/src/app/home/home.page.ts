9211595af8493ddc9a0ab227306d0e33a6506688
JuniorCriste <informaticode@gmail.com>
2021-11-23 21:42:52 -0300

    0.01

--- todo/src/app/home/home.page.ts
+++ todo/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
+
+  async presentAlertPromptAdd() {
+    const alert = await this.alertController.create({
+       header: 'Adicionar Tarefa!',
+      inputs: [
+        {
+          name: 'tarefa',
+          type: 'text',
+          placeholder: 'Tarefa'
+        },
+        {
+          name: 'data',
+          type: 'date',
+          min: '2021-01-01',
+          max: '2025-12-31'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        }, {
+          text: 'Salvar',
+          handler: () => {
+            console.log('Tarefa adicionada!');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
+
+
 
 }
+
