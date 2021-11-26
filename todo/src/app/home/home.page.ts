8e907321630b282f2a5cb90abb0abcc70c9774c6
JuniorCriste <informaticode@gmail.com>
2021-11-26 20:48:54 -0300

    0.05

--- todo/src/app/home/home.page.ts
+++ todo/src/app/home/home.page.ts
@@ -141,12 +141,12 @@ export class HomePage {
   async presentPopover(ev: any) {
     const popover = await this.popoverController.create({
       component: PopoverComponent,
-      cssClass: 'my-custom-class',
       event: ev,
       translucent: true
     });
     return await popover.present();
 
+}
 
 
 }
