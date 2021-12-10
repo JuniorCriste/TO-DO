2105f4a9a3cd5f6259d2a1e3dc871f7be7caf25f
JuniorCriste <informaticode@gmail.com>
2021-12-09 22:29:47 -0300

    4.0

--- todo/src/app/home/home.page.ts
+++ todo/src/app/home/home.page.ts
@@ -171,13 +171,44 @@ export async function interstitial(): Promise<void> {
   });
 
   const options: AdOptions = {
-    adId: 'YOUR ADID',
+    adId: 'ca-app-pub-5008288203998795/2233957738',
     // isTesting: true
     // npa: true
   };
   await AdMob.prepareInterstitial(options);
   await AdMob.showInterstitial();
 }
+
+
+// fim ADMOB
+
+export async function initialize(): Promise<void> {
+  const { status } = await AdMob.trackingAuthorizationStatus();
+
+  if (status === 'notDetermined') {
+    /**
+     * If you want to explain TrackingAuthorization before showing the iOS dialog,
+     * you can show the modal here.
+     * ex)
+     * const modal = await this.modalCtrl.create({
+     *   component: RequestTrackingPage,
+     * });
+     * await modal.present();
+     * await modal.onDidDismiss();  // Wait for close modal
+     **/
+  }
+ 
+  AdMob.initialize({
+    requestTrackingAuthorization: true,
+    testingDevices: ['2077ef9a63d2b398840261c8221a0c9b'],
+    initializeForTesting: true,
+  });
+}
+
+
+// inicio ADMOB
+
+
 
 
 // fim ADMOB
