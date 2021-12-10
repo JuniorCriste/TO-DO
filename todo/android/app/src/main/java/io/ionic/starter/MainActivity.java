d2e65fccf33986f4e22cf991bf623fde5259a743
JuniorCriste <informaticode@gmail.com>
2021-12-09 21:38:19 -0300

    4.0

--- todo/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ todo/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -3,10 +3,12 @@ package io.ionic.starter;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
- //ADMOB AXXIS
+ //início ADMOB
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         registerPlugin(com.getcapacitor.community.admob.AdMob.class);
     }
+
+//fim ADMOB
 }
